d0e5e15f195360fb541b986cb7c28e204b266678
Guilherme <global.guilherme@sage.com>
2019-09-30 08:51:32 -0300

    Primeiro Commit

---
+++ WebContent/resources/js/script-menu.js
@@ -0,0 +1,11 @@
+//$(document).ready(function () {
+// 
+//  $('[data-toggle="offcanvas"]').click(function () {
+//        $('#wrapper').toggleClass('toggled');
+//  });  
+//});
+//
+//window.onload = function() {
+//  $('#wrapper').toggleClass('toggled');
+//}
+//
